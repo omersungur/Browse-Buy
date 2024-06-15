268fdf5ab69e3413d6e56232cda7434c39303b10
Ömer Sungur <omersungur615@gmail.com>
2024-06-15 23:33:23 +0300

    Add theme resources

---
+++ compose-ui/src/main/java/com/omersungur/compose_ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.omersungur.compose_ui.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+@Stable
+val Color.Companion.Primary: Color
+    get() = Color(0xFF093D97)
+
+@Stable
+val Color.Companion.C_F4F7FF: Color
+    get() = Color(0xFFF4F7FF)
