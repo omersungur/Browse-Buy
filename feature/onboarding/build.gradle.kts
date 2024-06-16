970e81f0be40a28f1dca61f6336f098e0314b32c
Ömer Sungur <omersungur615@gmail.com>
2024-06-16 23:17:38 +0300

    Create OnboardingActivity

--- feature/onboarding/build.gradle.kts
+++ feature/onboarding/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -12,6 +13,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -27,14 +31,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+    implementation(project(":compose-ui"))
+    implementation(project(":domain"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
---
+++ feature/onboarding/src/main/java/com/omersungur/onboarding/OnboardingActivity.kt
@@ -0,0 +1,42 @@
+package com.omersungur.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.omersungur.compose_ui.theme.BrowseAndBuyAppTheme
+import com.omersungur.domain.util.SharedPref
+import com.omersungur.onboarding.navigation.OnboardingNavGraph
+
+class OnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            val navController = rememberNavController()
+
+            BrowseAndBuyAppTheme {
+                Scaffold { paddingValues ->
+                    Surface(modifier = Modifier.padding(paddingValues)) {
+                        OnboardingNavGraph(
+                            navController = navController
+                        ) {
+                            intentToAuth()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun intentToAuth() {
+        SharedPref(this).saveOnboardingShowingState()
+        // TODO: Intent to Auth Activity
+    }
+}
