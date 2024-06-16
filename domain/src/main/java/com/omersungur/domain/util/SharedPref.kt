4f026d8cf23547de960145e85feb618069c332f1
Ömer Sungur <omersungur615@gmail.com>
2024-06-16 07:47:12 +0300

    Add logged in logic with shared pref

--- domain/src/main/java/com/omersungur/domain/util/SharedPref.kt
+++ domain/src/main/java/com/omersungur/domain/util/SharedPref.kt
@@ -13,10 +13,19 @@ class SharedPref(context: Context) {
         editor.apply()
     }
 
+    fun saveLoggedInState() {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(KEY_OF_LOGGED_IN_STATE, false)
+        editor.apply()
+    }
+
     fun isShowingOnboardingScreen(): Boolean = sharedPreferences.getBoolean(KEY_OF_ONBOARDING_SHOWING_STATE, true)
+
+    fun isLoggedIn(): Boolean = sharedPreferences.getBoolean(KEY_OF_LOGGED_IN_STATE, true)
 
     companion object {
         private const val KEY_OF_SHARED_PREF = "MySharedPref"
         private const val KEY_OF_ONBOARDING_SHOWING_STATE = "onboardingShowingPrefKey"
+        private const val KEY_OF_LOGGED_IN_STATE = "loggedInPrefKey"
     }
 }
