2b9b5d87bd70c08f9c2b845af49f46c8959735d3
Ömer Sungur <omersungur615@gmail.com>
2024-06-18 18:00:42 +0300

    Remove repository provider

--- data/src/main/java/com/omersungur/data/di/FirebaseAuthenticationModule.kt
+++ data/src/main/java/com/omersungur/data/di/FirebaseAuthenticationModule.kt
@@ -1,12 +1,22 @@
 package com.omersungur.data.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.google.firebase.auth.FirebaseAuth
+import com.omersungur.data.remote.ProductApi
 import com.omersungur.data.repository.FirebaseAuthenticationRepositoryImpl
+import com.omersungur.data.repository.JWTAuhRepositoryImpl
 import com.omersungur.domain.repository.auth.FirebaseAuthenticationRepository
+import com.omersungur.domain.repository.auth.JWTAuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -16,10 +26,4 @@ object FirebaseAuthenticationModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
-
-    @Provides
-    @Singleton
-    fun provideAuthenticationRepository(
-        auth: FirebaseAuth
-    ): FirebaseAuthenticationRepository = FirebaseAuthenticationRepositoryImpl(auth)
 }
