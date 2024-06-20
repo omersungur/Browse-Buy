1a57833b0ce5a6c1fce8193af263a25a1bdbe841
Ömer Sungur <omersungur615@gmail.com>
2024-06-21 01:47:11 +0300

    Create profile module

--- feature/profile/src/main/java/com/omersungur/profile/ProfileFragment.kt
+++ feature/profile/src/main/java/com/omersungur/profile/ProfileFragment.kt
@@ -1,6 +1,5 @@
-package com.omersungur.home.ui.profile
+package com.omersungur.profile
 
 import androidx.fragment.app.Fragment
-import com.omersungur.home.R
 
 class ProfileFragment : Fragment(R.layout.fragment_profile)
