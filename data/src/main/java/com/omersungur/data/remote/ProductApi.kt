179aab17b5582146d63cd77c4ce385046686b2f2
Ömer Sungur <omersungur615@gmail.com>
2024-06-21 16:05:44 +0300

    Add getProducts function

--- data/src/main/java/com/omersungur/data/remote/ProductApi.kt
+++ data/src/main/java/com/omersungur/data/remote/ProductApi.kt
@@ -2,11 +2,16 @@ package com.omersungur.data.remote
 
 import com.omersungur.data.remote.dto.jwt_user.JWTDataDto
 import com.omersungur.data.remote.dto.jwt_user.JWTUserDto
+import com.omersungur.data.remote.dto.product.ProductDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ProductApi {
 
     @POST("auth/login")
     suspend fun login(@Body jwtUser: JWTUserDto): JWTDataDto
+
+    @GET("products")
+    suspend fun getProducts(): ProductDto
 }
