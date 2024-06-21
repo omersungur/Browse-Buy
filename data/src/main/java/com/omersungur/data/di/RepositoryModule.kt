e670785132e92a9e555a31daacf0a6debcbc769c
Ömer Sungur <omersungur615@gmail.com>
2024-06-21 16:06:50 +0300

    Provide ProductRepository

--- data/src/main/java/com/omersungur/data/di/RepositoryModule.kt
+++ data/src/main/java/com/omersungur/data/di/RepositoryModule.kt
@@ -4,13 +4,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.omersungur.data.remote.ProductApi
 import com.omersungur.data.repository.FirebaseAuthenticationRepositoryImpl
 import com.omersungur.data.repository.JWTAuhRepositoryImpl
+import com.omersungur.data.repository.product.ProductRepositoryImpl
 import com.omersungur.domain.repository.auth.FirebaseAuthenticationRepository
 import com.omersungur.domain.repository.auth.JWTAuthRepository
+import com.omersungur.domain.repository.product.ProductRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
@@ -26,4 +29,10 @@ object RepositoryModule {
     fun provideAuthenticationRepository(
         auth: FirebaseAuth
     ): FirebaseAuthenticationRepository = FirebaseAuthenticationRepositoryImpl(auth)
+
+    @Provides
+    @Singleton
+    fun provideProductRepository(
+        productApi: ProductApi
+    ): ProductRepository = ProductRepositoryImpl(productApi)
 }
