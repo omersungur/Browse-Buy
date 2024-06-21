9a602a733c7b017798e490ec4698d368b9b81a47
Ömer Sungur <omersungur615@gmail.com>
2024-06-21 16:08:08 +0300

    Fetch data

--- feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
+++ feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
@@ -1,6 +1,30 @@
 package com.omersungur.home.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.omersungur.home.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                if (state.isSuccess) {
+                    state.products.forEach {
+                        println(it.title)
+                    }
+                }
+            }
+        }
+    }
+}
