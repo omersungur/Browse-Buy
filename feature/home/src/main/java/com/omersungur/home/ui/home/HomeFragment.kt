bbe95482f47e2ab869fc560591983e2536f97987
Ömer Sungur <omersungur615@gmail.com>
2024-06-21 18:21:28 +0300

    Collect categories

--- feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
+++ feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.omersungur.home.ui.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.omersungur.home.R
+import com.omersungur.home.databinding.FragmentHomeBinding
+import com.omersungur.home.ui.home.adapter.CategoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -13,6 +18,18 @@ import kotlinx.coroutines.launch
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val viewModel: HomeViewModel by viewModels()
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,8 +37,11 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         lifecycleScope.launch {
             viewModel.uiState.collect { state ->
                 if (state.isSuccess) {
-                    state.products.forEach {
-                        println(it.title)
+                    categoryAdapter = CategoryAdapter(state.categories)
+                    binding.rvCategory.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                    binding.rvCategory.adapter = categoryAdapter
+                    state.categories.forEach {
+                        println(it.name)
                     }
                 }
             }
