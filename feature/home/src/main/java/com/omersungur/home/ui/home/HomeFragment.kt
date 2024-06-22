2fbb995872db3c6a1c21794209a6d213a995c1f4
Ömer Sungur <omersungur615@gmail.com>
2024-06-22 23:22:35 +0300

    Create HomeScreen and fetch data

--- feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
+++ feature/home/src/main/java/com/omersungur/home/ui/home/HomeFragment.kt
@@ -4,51 +4,373 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.sharp.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.omersungur.compose_ui.theme.BrowseAndBuyAppTheme
+import com.omersungur.domain.model.category.Category
+import com.omersungur.domain.model.product.ProductX
 import com.omersungur.home.R
-import com.omersungur.home.databinding.FragmentHomeBinding
-import com.omersungur.home.ui.home.adapter.CategoryAdapter
-import com.omersungur.home.ui.home.adapter.ProductAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.*
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home) {
-
-    private val viewModel: HomeViewModel by viewModels()
-    private lateinit var categoryAdapter: CategoryAdapter
-    private lateinit var productAdapter: ProductAdapter
-    private lateinit var binding: FragmentHomeBinding
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val view = binding.root
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+            setContent {
+                BrowseAndBuyAppTheme {
+                    Scaffold {
+                        Surface(modifier = Modifier.padding(it)) {
+                            HomeScreen()
+                        }
+                    }
+                }
+            }
+        }
         return view
     }
+}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    var text by remember { mutableStateOf("") }
+    val uiState by viewModel.uiState.collectAsState()
+    var debounceJob by remember { mutableStateOf<Job?>(null) }
 
-        lifecycleScope.launch {
-            viewModel.uiState.collect { state ->
-                if (state.isSuccess) {
-                    categoryAdapter = CategoryAdapter(state.categories)
-                    binding.rvCategory.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-                    binding.rvCategory.adapter = categoryAdapter
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 8.dp)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = "Discover",
+                fontWeight = FontWeight.Bold,
+                fontFamily = robotoFamily,
+                fontSize = 20.sp,
+            )
 
-                    productAdapter = ProductAdapter(state.products)
-                    binding.rvProduct.layoutManager = GridLayoutManager(requireContext(), 2)
-                    binding.rvProduct.adapter = productAdapter
+            Icon(
+                imageVector = Icons.Outlined.ShoppingCart,
+                contentDescription = "Shopping Cart",
+                tint = Color.Black,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            value = text,
+            onValueChange = {
+                text = it
+
+                debounceJob?.cancel()
+                debounceJob = CoroutineScope(Dispatchers.Main).launch {
+                    delay(500)
+                    viewModel.getProducts(it)
                 }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Sharp.Search,
+                    contentDescription = "Search Icon",
+                )
+            },
+            placeholder = {
+                Text(text = "Search")
+            },
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            painter = painterResource(id = R.drawable.banner1),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Banner Image",
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Categories",
+                fontWeight = FontWeight.Bold,
+                fontFamily = robotoFamily,
+                color = Color.Black,
+            )
+
+            Text(
+                text = "See All",
+                fontWeight = FontWeight.Bold,
+                fontFamily = robotoFamily,
+                color = Color.Blue,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        with(uiState) {
+            if (loadingState) {
+                CircularProgressIndicator()
+            }
+            if (isSuccess) {
+                CategoryLazyRow(categories = categories)
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                ProductLazyColumn(products = products)
             }
         }
     }
 }
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier,
+    categoryName: String,
+    onCategoryClick: (String) -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = { onCategoryClick(categoryName) },
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF008b8b))
+    ) {
+        Text(text = categoryName)
+    }
+}
+
+@Composable
+fun CategoryLazyRow(
+    modifier: Modifier = Modifier,
+    categories: List<Category>
+) {
+    LazyRow(
+        modifier = modifier.padding(start = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(categories) {
+            CategoryRow(categoryName = it.name.orEmpty()) { name ->
+                println(name)
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductLazyColumn(
+    modifier: Modifier = Modifier,
+    products: List<ProductX>,
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        items(products) { item ->
+            ProductCard(product = item)
+        }
+    }
+}
+
+@Composable
+fun ProductCard(
+    modifier: Modifier = Modifier,
+    product: ProductX,
+    onAddToCartClicked: () -> Unit = {},
+) {
+    val formattedRating = String.format(locale = Locale.getDefault(), "%.1f", product.rating)
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+        border = BorderStroke(width = 2.dp, color = Color(0xFFFFA500)),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            ) {
+
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "Favorite Icon",
+                    tint = Color.Red,
+                )
+
+                AsyncImage(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(product.thumbnail)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(com.omersungur.compose_ui.R.drawable.ic_google),
+                    contentDescription = product.title,
+                    contentScale = ContentScale.Fit,
+                )
+            }
+
+            Text(
+                text = product.title.orEmpty(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                overflow = TextOverflow.Ellipsis,
+                fontFamily = robotoFamily,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black,
+                maxLines = 1,
+            )
+
+            Text(
+                text = product.brand ?: "No Brand Found!",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                overflow = TextOverflow.Ellipsis,
+                fontFamily = robotoFamily,
+                color = Color.Black,
+                maxLines = 1,
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = "$${product.price}",
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    fontFamily = robotoFamily,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Black,
+                )
+
+                Row(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    Icon(
+                        imageVector = Icons.Outlined.Star,
+                        contentDescription = "Star Icon",
+                        tint = Color(0xFFFFA500),
+                    )
+
+                    Text(text = formattedRating)
+                }
+            }
+
+            Button(
+                onClick = onAddToCartClicked,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                    .align(CenterHorizontally),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFA500)
+                ),
+            ) {
+                Text(text = "Add to Cart")
+            }
+        }
+    }
+}
+
+val robotoFamily = FontFamily(
+    Font(R.font.roboto_bold, FontWeight.Light),
+    Font(R.font.roboto_light, FontWeight.Normal),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+)
