82e07027a0fd4b30f784be08439d2322899d1195
Ömer Sungur <omersungur615@gmail.com>
2024-06-24 16:16:25 +0300

    Create UserRepository and its implementation

---
+++ data/src/main/java/com/omersungur/data/repository/user/UserRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.omersungur.data.repository.user
+
+import com.omersungur.data.mapper.toUser
+import com.omersungur.data.remote.ProductApi
+import com.omersungur.domain.model.user.User
+import com.omersungur.domain.repository.user.UserRepository
+import com.omersungur.domain.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val productApi: ProductApi,
+) : UserRepository {
+
+    override fun getUserInfo(userId: Int): Flow<Resource<User>> = flow {
+        try {
+            emit(Resource.Loading())
+            val userInfo = productApi.getUserInfo(userId)
+            emit(Resource.Success(userInfo.toUser()))
+        } catch (ex: Exception) {
+            emit(Resource.Error(ex.localizedMessage ?: "Error"))
+        }
+    }
+}
---
+++ domain/src/main/java/com/omersungur/domain/repository/user/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.omersungur.domain.repository.user
+
+import com.omersungur.domain.model.user.User
+import com.omersungur.domain.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+
+    fun getUserInfo(userId: Int): Flow<Resource<User>>
+}
