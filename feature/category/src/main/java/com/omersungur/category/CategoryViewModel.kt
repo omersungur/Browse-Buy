91ad379fe35d9a4eebc4aef7013b4b77ce6540b5
Ömer Sungur <omersungur615@gmail.com>
2024-06-24 15:13:09 +0300

    Add getProductByCategory logic

--- feature/category/src/main/java/com/omersungur/category/CategoryViewModel.kt
+++ feature/category/src/main/java/com/omersungur/category/CategoryViewModel.kt
@@ -3,6 +3,7 @@ package com.omersungur.category
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.omersungur.domain.model.category.Category
+import com.omersungur.domain.model.product.ProductX
 import com.omersungur.domain.repository.product.ProductRepository
 import com.omersungur.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -57,11 +58,50 @@ class CategoryViewModel @Inject constructor(
         }
     }.launchIn(viewModelScope)
 
+    fun getProductsByCategory(categoryName: String) = productRepository.getProductsByCategory(categoryName).onEach { result ->
+        when (result) {
+            is Resource.Loading -> {
+                _uiState.update { state ->
+                    state.copy(loadingState = true)
+                }
+            }
+
+            is Resource.Success -> {
+                _uiState.update { state ->
+                    state.copy(
+                        loadingState = false,
+                        isHaveError = false,
+                        categorySuccess = true,
+                        categoryProducts = result.data?.products ?: emptyList()
+                    )
+                }
+            }
+
+            is Resource.Error -> {
+                _uiState.update { state ->
+                    state.copy(
+                        loadingState = false,
+                        isHaveError = true,
+                        errorMessage = result.errorMessage.orEmpty()
+                    )
+                }
+            }
+        }
+    }.launchIn(viewModelScope)
+
+    fun updateSuccessState() {
+        _uiState.update { state ->
+            state.copy(isSuccess = false)
+        }
+    }
+
     data class CategoryUIState(
         val loadingState: Boolean = false,
         val isHaveError: Boolean = false,
         val isSuccess: Boolean = false,
         val errorMessage: String = "",
         val categories : List<Category> = emptyList(),
+        val categoryProducts : List<ProductX> = emptyList(),
+        val categorySuccess : Boolean = false
     )
 }
