5523d2b70cfa688de326df0da5cbf25cdbfa1bbb
Ömer Sungur <omersungur615@gmail.com>
2024-06-24 15:11:44 +0300

    Add cart and productByCategory endpoints

--- data/src/main/java/com/omersungur/data/remote/ProductApi.kt
+++ data/src/main/java/com/omersungur/data/remote/ProductApi.kt
@@ -1,5 +1,6 @@
 package com.omersungur.data.remote
 
+import com.omersungur.data.remote.dto.cart.CartDto
 import com.omersungur.data.remote.dto.category.CategoryDto
 import com.omersungur.data.remote.dto.jwt_user.JWTDataDto
 import com.omersungur.data.remote.dto.jwt_user.JWTUserDto
@@ -7,6 +8,7 @@ import com.omersungur.data.remote.dto.product.ProductDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ProductApi {
 
@@ -18,4 +20,10 @@ interface ProductApi {
 
     @GET("products/categories")
     suspend fun getCategories(): List<CategoryDto>
+
+    @GET("carts/user/{userId}")
+    suspend fun getUserCart(@Path("userId") userId: Int): CartDto
+
+    @GET("products/category/{categoryName}")
+    suspend fun getProductsByCategory(@Path("categoryName") categoryName: String): ProductDto
 }
