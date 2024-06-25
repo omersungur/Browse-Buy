4c137c54b8ac4c5bb48ab148731d2437084fb381
Ömer Sungur <omersungur615@gmail.com>
2024-06-25 20:00:53 +0300

    Create Cart Mapper

--- data/src/main/java/com/omersungur/data/mapper/CartMapper.kt
+++ data/src/main/java/com/omersungur/data/mapper/CartMapper.kt
@@ -1,9 +1,13 @@
 package com.omersungur.data.mapper
 
+import com.omersungur.data.remote.dto.cart.AddCartRequestDto
 import com.omersungur.data.remote.dto.cart.CartDto
+import com.omersungur.data.remote.dto.cart.CartProductDto
 import com.omersungur.data.remote.dto.cart.CartXDto
 import com.omersungur.data.remote.dto.cart.ProductCartDto
+import com.omersungur.domain.model.cart.AddCartRequest
 import com.omersungur.domain.model.cart.Cart
+import com.omersungur.domain.model.cart.CartProduct
 import com.omersungur.domain.model.cart.CartX
 import com.omersungur.domain.model.cart.ProductCart
 
@@ -38,5 +42,19 @@ fun ProductCartDto.toProduct(): ProductCart {
         thumbnail = thumbnail,
         title = title,
         total = total,
+    )
+}
+
+fun AddCartRequest.toAddCartRequestDto(): AddCartRequestDto {
+    return AddCartRequestDto(
+        userId = userId,
+        products = products.map { it.toCartProductDto() }
+    )
+}
+
+fun CartProduct.toCartProductDto(): CartProductDto {
+    return CartProductDto(
+        id = id,
+        quantity = quantity
     )
 }
