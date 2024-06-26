1d258ec974c7e10717d66eed377583b85d2a4cfe
Ömer Sungur <omersungur615@gmail.com>
2024-06-26 19:09:05 +0300

    Add remote config logic

--- feature/auth/src/main/java/com/omersungur/auth/AuthenticationActivity.kt
+++ feature/auth/src/main/java/com/omersungur/auth/AuthenticationActivity.kt
@@ -1,6 +1,8 @@
 package com.omersungur.auth
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +12,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.google.android.play.integrity.internal.ac
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 import com.omersungur.auth.navigation.AuthenticationNavGraph
 import com.omersungur.compose_ui.theme.BrowseAndBuyAppTheme
 import com.omersungur.domain.util.goToTheActivity
@@ -21,10 +32,59 @@ class AuthenticationActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        FirebaseApp.initializeApp(this)
+
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3000
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.e("Data", "Config params updated: $updated")
+                    Toast.makeText(
+                        this,
+                        "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Fetch failed",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
+        remoteConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) {
+                Log.e("onUpdate", "Updated keys: " + configUpdate.updatedKeys)
+
+                if (configUpdate.updatedKeys.contains("backgroundColor")) {
+                    remoteConfig.activate().addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val color = FirebaseRemoteConfig.getInstance().getString("backgroundColor")
+                            Log.d("Pull Color", color)
+                        }
+                    }
+                }
+            }
+
+            override fun onError(error: FirebaseRemoteConfigException) {
+                Log.w("onError", "Config update error with code: " + error.code, error)
+            }
+        })
+
         setContent {
             val navController = rememberNavController()
 
             BrowseAndBuyAppTheme {
+
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(modifier = Modifier.padding(innerPadding)) {
                         AuthenticationNavGraph(navController = navController) {
